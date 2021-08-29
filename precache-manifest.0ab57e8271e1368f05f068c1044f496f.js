1a0a77c20fa1654914304a97efea82642ef03b26
Aleksandr784 <digiprotekt@gmale.com>
2021-08-29 17:33:15 +0300

    Updates

---
+++ precache-manifest.0ab57e8271e1368f05f068c1044f496f.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4078d1b5055450295615a8caf65209a9",
+    "url": "/colorit/index.html"
+  },
+  {
+    "revision": "822e2f292cfefba59f03",
+    "url": "/colorit/static/css/main.3dfd8b7e.chunk.css"
+  },
+  {
+    "revision": "8eb8ad2b7040b872280d",
+    "url": "/colorit/static/js/2.869e0b5f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/colorit/static/js/2.869e0b5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "822e2f292cfefba59f03",
+    "url": "/colorit/static/js/main.37a0d00a.chunk.js"
+  },
+  {
+    "revision": "41e919f1ea552a18571d",
+    "url": "/colorit/static/js/runtime-main.84f1a171.js"
+  },
+  {
+    "revision": "7beaecd4df6847249408bdf7ca80cffd",
+    "url": "/colorit/static/media/b1.7beaecd4.jpg"
+  },
+  {
+    "revision": "cc5f010fbb16d8d0da44db2c09ec8838",
+    "url": "/colorit/static/media/b2.cc5f010f.jpg"
+  },
+  {
+    "revision": "406f34cbb34f43ffa364596795f53e25",
+    "url": "/colorit/static/media/b3.406f34cb.jpg"
+  },
+  {
+    "revision": "242dd3c8ff1e21534affa4a0771c7ea6",
+    "url": "/colorit/static/media/b4.242dd3c8.jpg"
+  },
+  {
+    "revision": "6d0ea5267b1dddaedbab3f96da06a057",
+    "url": "/colorit/static/media/b5.6d0ea526.jpg"
+  },
+  {
+    "revision": "e3286e0b69cbb68ad29805646478d404",
+    "url": "/colorit/static/media/b6.e3286e0b.jpg"
+  },
+  {
+    "revision": "f0fe3174086093c697cf339b6ff3dca5",
+    "url": "/colorit/static/media/b7.f0fe3174.jpg"
+  },
+  {
+    "revision": "661cab412f667f88e84e3e52d0cb2f8f",
+    "url": "/colorit/static/media/b8.661cab41.jpg"
+  },
+  {
+    "revision": "8160c9c92ae10c3da32216cfab36fa9a",
+    "url": "/colorit/static/media/d2.8160c9c9.jpg"
+  },
+  {
+    "revision": "43f63e10bb159e0ed9f7d7eec17e8c69",
+    "url": "/colorit/static/media/d3.43f63e10.jpg"
+  },
+  {
+    "revision": "e9a2e9f96b5cda8e9d7916356b116ea9",
+    "url": "/colorit/static/media/d4.e9a2e9f9.jpg"
+  },
+  {
+    "revision": "1e8feded81ea0703f2b72a631d01282f",
+    "url": "/colorit/static/media/d5.1e8feded.jpg"
+  },
+  {
+    "revision": "10b6158e167fd2ea1847e0dcf2c358fe",
+    "url": "/colorit/static/media/e10.10b6158e.jpg"
+  },
+  {
+    "revision": "03759d0b823d0c15383ecefac559aa4d",
+    "url": "/colorit/static/media/e11.03759d0b.jpg"
+  },
+  {
+    "revision": "3f871636d7df2da53a596e590563f84c",
+    "url": "/colorit/static/media/e12.3f871636.jpg"
+  },
+  {
+    "revision": "fbee73547bdbc0f53f07b98cc4a5a2c9",
+    "url": "/colorit/static/media/e13.fbee7354.jpg"
+  },
+  {
+    "revision": "efcf12e22c29410b343cceeefe1a56f4",
+    "url": "/colorit/static/media/e2.efcf12e2.jpg"
+  },
+  {
+    "revision": "14832331f706e6ddb8b0c38035fdf6cd",
+    "url": "/colorit/static/media/e3.14832331.jpg"
+  },
+  {
+    "revision": "d302d4cf00eca5b2fa7f721eb3cd8480",
+    "url": "/colorit/static/media/e4.d302d4cf.jpg"
+  },
+  {
+    "revision": "6f1a1f3e31b22320ea68581e77c9afef",
+    "url": "/colorit/static/media/e5.6f1a1f3e.jpg"
+  },
+  {
+    "revision": "c7aabe212b04488489f7106568477aec",
+    "url": "/colorit/static/media/e6.c7aabe21.jpg"
+  },
+  {
+    "revision": "f34b452607b25ae70d09f25badbdcfb5",
+    "url": "/colorit/static/media/e7.f34b4526.jpg"
+  },
+  {
+    "revision": "558b0508638276e7ab976092449c9797",
+    "url": "/colorit/static/media/e8.558b0508.jpg"
+  },
+  {
+    "revision": "f2a06d3b5ed1c2a2e29bcd6c8d4db5ed",
+    "url": "/colorit/static/media/e9.f2a06d3b.jpg"
+  },
+  {
+    "revision": "8a83953adc6e46d04c24364e486db9b6",
+    "url": "/colorit/static/media/f1.8a83953a.jpg"
+  },
+  {
+    "revision": "fc033aa1c6c2b586d125dd12e1260a9c",
+    "url": "/colorit/static/media/f2.fc033aa1.jpg"
+  },
+  {
+    "revision": "54c975345d141223997efc57883b585a",
+    "url": "/colorit/static/media/f3.54c97534.jpg"
+  },
+  {
+    "revision": "d55bc8c5fdb9a87e6b90c29e87dd820a",
+    "url": "/colorit/static/media/f4.d55bc8c5.jpg"
+  },
+  {
+    "revision": "e4fad0b0cb9fd14a5431e69e9e668b02",
+    "url": "/colorit/static/media/f5.e4fad0b0.jpg"
+  },
+  {
+    "revision": "e49443167854354471293fb4b567b3c2",
+    "url": "/colorit/static/media/f6.e4944316.jpg"
+  },
+  {
+    "revision": "7f0e5d4ee0653c4e8ee0a66692aea1f1",
+    "url": "/colorit/static/media/f7.7f0e5d4e.jpg"
+  },
+  {
+    "revision": "1dcd0466ad8cad9f70530069223d9dcc",
+    "url": "/colorit/static/media/f8.1dcd0466.jpg"
+  },
+  {
+    "revision": "3a4408522168fe59340c21daa176b3ee",
+    "url": "/colorit/static/media/i1.3a440852.jpg"
+  },
+  {
+    "revision": "c5b7fc38fbdfcab2756368feee4957bd",
+    "url": "/colorit/static/media/i2.c5b7fc38.jpg"
+  },
+  {
+    "revision": "406f34cbb34f43ffa364596795f53e25",
+    "url": "/colorit/static/media/i3.406f34cb.jpg"
+  },
+  {
+    "revision": "0121ed48be015ac92032a879c76a5697",
+    "url": "/colorit/static/media/i4.0121ed48.jpg"
+  },
+  {
+    "revision": "6fc4b8b7bef085ecd2918a6103da38fb",
+    "url": "/colorit/static/media/i5.6fc4b8b7.jpg"
+  },
+  {
+    "revision": "d55bc8c5fdb9a87e6b90c29e87dd820a",
+    "url": "/colorit/static/media/i6.d55bc8c5.jpg"
+  },
+  {
+    "revision": "613eef42d6a403eec3520a4c5dd0713b",
+    "url": "/colorit/static/media/instagram1.613eef42.png"
+  },
+  {
+    "revision": "347b228c9c0ddf54a6a42528e0f3c74d",
+    "url": "/colorit/static/media/k1.347b228c.jpg"
+  },
+  {
+    "revision": "292dcf0d608981e8b94928fedb0973bf",
+    "url": "/colorit/static/media/k2.292dcf0d.jpg"
+  },
+  {
+    "revision": "fd1f0f0bf6f4a5ec29464e0c8826f33e",
+    "url": "/colorit/static/media/n2.fd1f0f0b.jpg"
+  },
+  {
+    "revision": "3060a288ab016a91b5f3a788dec53b83",
+    "url": "/colorit/static/media/n3.3060a288.jpg"
+  },
+  {
+    "revision": "6c7734a02d2a2bf2b577bc02241bd0a1",
+    "url": "/colorit/static/media/n4.6c7734a0.jpg"
+  },
+  {
+    "revision": "bdc7bc54b3718bc50271c87060f85fa3",
+    "url": "/colorit/static/media/n5.bdc7bc54.jpg"
+  }
+]);
